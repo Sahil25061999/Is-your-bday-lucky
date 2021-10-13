eaf5a18aef2b42a296325e6ccc1f147b3d3e680c
Sahil Padvi <sahilpadvi25@gmail.com>
2021-10-13 17:23:41 +0530

    bug fix

--- script.js
+++ script.js
@@ -6,15 +6,14 @@ const display = document.querySelector('.display');
 
 check.addEventListener('click', () => {
   display.style.display = 'none';
-  if (lucky.value > 0) {
+  if (lucky.value > 0 && bdate.value != '') {
     checkBday(bdate.value);
   } else {
-    displayMessage('Enter valid number');
+    displayMessage('Enter positive number and fill the required fields');
   }
 });
 
 function checklucky(sum1) {
-  console.log(sum1);
   if (sum1 % lucky.value == 0) {
     displayMessage('Your bday is lucky');
   } else {
@@ -23,12 +22,10 @@ function checklucky(sum1) {
 }
 
 function checkBday(bdate) {
-  console.log(bdate);
   const birthDigit = bdate.replaceAll('-', '');
   var sum1 = 0;
   for (let i = 0; i < birthDigit.length; i++) {
     sum1 += Number(birthDigit[i]);
-    // console.log(birthDigit[i]);
   }
   checklucky(sum1);
 }
